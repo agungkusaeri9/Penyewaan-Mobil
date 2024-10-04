ac0731041c1b9c04f1f28af2f51d0d1090a8711d
agungkusaeri9 <agung.kusaeri9@gmail.com>
2024-10-04 20:37:32 +0700

    fix: icon sidebar dan footer

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -8,25 +8,25 @@
         </li>
         <li class="nav-item">
             <a class="nav-link py-2" href="{{ route('admin.mobil.index') }}">
-                <i class="mdi mdi-database  pr-2 icon-large"></i>
+                <i class="mdi mdi-car  pr-2 icon-large"></i>
                 <span class="menu-title">Mobil</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link py-2" href="{{ route('admin.merk.index') }}">
-                <i class="mdi mdi-database  pr-2 icon-large"></i>
+                <i class="mdi mdi-label  pr-2 icon-large"></i>
                 <span class="menu-title">Merk</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link py-2" href="{{ route('admin.metode-pembayaran.index') }}">
-                <i class="mdi mdi-database  pr-2 icon-large"></i>
+                <i class="mdi mdi-bank  pr-2 icon-large"></i>
                 <span class="menu-title">Metode Pembayaran</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link py-2" href="{{ route('admin.customer.index') }}">
-                <i class="mdi mdi-database  pr-2 icon-large"></i>
+                <i class="mdi mdi-account  pr-2 icon-large"></i>
                 <span class="menu-title">Customer</span>
             </a>
         </li>
